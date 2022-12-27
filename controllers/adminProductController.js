f0d5451c2a4ade6ab35bad60e3a19db5954af0b5
HQDuy47 <72744215+HQDuy47@users.noreply.github.com>
2022-12-27 16:54:33 +0700

    sort-admin-product

--- controllers/adminProductController.js
+++ controllers/adminProductController.js
@@ -4,9 +4,24 @@ const authService = require('../model/authService')
 let getProductManage = async (req, res) => {
     const { AVATAR: ava } = await authService.getUserByID(res.locals.user.id);
     //  let idProduct = req.params.id;
-    const listProduct = await adminProductService.getAllProduct();
+    const getProduct = await adminProductService.getAllProduct();
     //console.log(listProduct);
-    return res.render('product-manage.ejs', { ava, listProduct: listProduct })
+    let listProduct;
+    const {
+        sortName: sortName,
+        sortType: sortType,
+        sortNumbuy: sortNumbuy,
+        sortCreateon: sortCreateon, 
+        sort: sortFilter
+
+    } = req.query;
+
+    if (sortName || sortType || sortNumbuy || sortCreateon || sortFilter) {
+        listProduct = await adminProductService.getSortProduct(req.query);
+    }
+    else listProduct = getProduct;
+    const originUrl = `?${req.baseUrl}`;
+    return res.render('product-manage.ejs', { ava, listProduct: listProduct, originUrl: originUrl })
 }
 let getDetailsProduct = async (req, res) => {
     const { AVATAR: ava } = await authService.getUserByID(res.locals.user.id);
@@ -17,6 +32,29 @@ let getDetailsProduct = async (req, res) => {
 
     return res.render('details-product.ejs', { ava, details: details })
 }
+
+//SORT PRODUCT-MANAGE
+// let getUsersManage = async (req, res) => {
+//     const { AVATAR: ava } = await authService.getUserByID(res.locals.user.id);
+
+//     const getUser = await adminUserService.getAllUser();
+//     //console.log(list.length);
+//     let listUser;
+//     const {
+//         timeCreate: timeCreate,
+//         sortEmail: sortEmail,
+//         sortHoTen: sortName,
+//         sort: sortFilter
+
+//     } = req.query;
+//     if (timeCreate || sortEmail || sortName || sortFilter) {
+//         listUser = await adminUserService.getSortUser(req.query);
+//     }
+//     else listUser = getUser;
+//     const originUrl = `?${req.baseUrl}`;
+//     return res.render('users-manage.ejs', { listUser: listUser, ava: ava, originUrl: originUrl });
+// }
+/////////////////
 
 module.exports = {
     getProductManage,
--- model/adminProductService.js
+++ model/adminProductService.js
@@ -11,7 +11,49 @@ let getAllProduct = async () => {
 
     return result[0];
 }
+//SORT PRODUCT-MANAGE
+let getSortProduct = async (queryFilter) => {
+    const {
+        sortName: sortName,
+        sortType: sortType,
+        sortNumbuy: sortNumbuy,
+        sortCreateon: sortCreateon, 
+        sort: sortFilter
+    } = queryFilter;
+    let values = [];
+    let sql = 'SELECT pd.IDPRODUCT,pd.NAMEPRODUCT, tp.NAMETYPE, pd.CREATEON, pd.PRICE, pd.NUMBUY FROM onlineshop.product pd JOIN onlineshop.type tp on tp.IDTYPE = pd.IDTYPE';
+
+    if (sortFilter && typeof sortFilter === 'string' && (sortName || sortType || sortNumbuy || sortCreateon)) {
+        //sort tăng dần
+        if (typeof sortName === 'string') {
+            sql += ' ORDER BY pd.NAMEPRODUCT';
+            values.push(parseInt(sortName))
+        }
+        if (typeof sortType === 'string') {
+            sql += ' ORDER BY tp.NAMETYPE';
+            values.push(parseFloat(sortType))
+        }
+        if (typeof sortNumbuy === 'string') {
+            sql += ' ORDER BY pd.NUMBUY';
+            values.push(parseFloat(sortType))
+        }
+        else if (typeof sortCreateon === 'string') {
+            sql += ' ORDER BY pd.CREATEON';
+            values.push(parseInt(sortCreateon))
+        }
+        //sort giảm dần
+        if (sortFilter === 'down') {
+            sql += ' DESC';
+        }
+    }
+    const result = await db.query(sql, values);
+    console.log(result);
+    return result[0];
+
+}
+
 module.exports = {
     getAllProduct,
-    getProduct
+    getProduct,
+    getSortProduct
 }
